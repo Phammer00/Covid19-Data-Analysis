f6a4dee2bf522d0d74f277671bad9f937151d1a0
Phammer00 <hamm_pascal@gmx.de>
2025-03-18 21:19:57 +0100

    covid_analysis.py

--- covid_analysis.py
+++ covid_analysis.py
@@ -2,7 +2,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 # 1) Load dataset
-df = pd.read_csv("C:/Users/Hammp/Desktop/covid_de.csv")
+file_path = os.path.join(os.getcwd(), "covid_de.csv")
+df = pd.read_csv(file_path)
 print("Dataset loaded successfully:")
 print(df.head())
 
